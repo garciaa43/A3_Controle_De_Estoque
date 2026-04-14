423c5e22f8c04976f9877b165020c5ec40786dc9
garciaa43 <gabrielcordeir.gg@gmail.com>
2026-04-14 13:59:46 -0300

    feat: classe Movimentacao e mudança de nomes de package e a main class

--- src/main/java/main/main.java
+++ src/main/java/main/main.java
@@ -2,13 +2,13 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  */
 
-package com.mycompany.a3_controle_de_estoque;
+package main;
 
 /**
  *
  * @author Home Office
  */
-public class A3_Controle_De_Estoque {
+public class main {
 
     public static void main(String[] args) {
         System.out.println("Hello World!");
